77e6d61d1b2d0c35def624b1ef0386ab7ee86d44
Adrian Pionka <adrian@pionka.com>
2023-05-22 10:58:33 +0200

    add PasswordConfirmValidatorTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.7"
-	id("io.spring.dependency-management") version "1.1.0"
-	id("com.vaadin") version "24.0.5"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+    id("org.springframework.boot") version "3.0.7"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("com.vaadin") version "24.0.5"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+    kotlin("kapt") version "1.8.21"
 }
 
 group = "pl.wydarzeniawokolicy"
@@ -13,49 +14,51 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 extra["vaadinVersion"] = "24.0.5"
 extra["mapstructVersion"] = "1.5.5.Final"
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("org.springframework.boot:spring-boot-starter-mail")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("com.vaadin:vaadin-spring-boot-starter")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.mapstruct:mapstruct:${property("mapstructVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.vaadin:vaadin-spring-boot-starter")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.mapstruct:mapstruct:${property("mapstructVersion")}")
+    kapt("org.mapstruct:mapstruct-processor:${property("mapstructVersion")}")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
-	testRuntimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testRuntimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
-	}
+    imports {
+        mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
---
+++ src/test/kotlin/pl/wydarzeniawokolicy/api/users/PasswordConfirmValidatorTest.kt
@@ -0,0 +1,61 @@
+package pl.wydarzeniawokolicy.api.users
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PasswordConfirmValidatorTest {
+
+    private val passwordConfirmValidator = PasswordConfirmValidator()
+
+    @Test
+    fun shouldReturnValidIfUserDetailsDtoIsNull() {
+        //when
+        val valid = passwordConfirmValidator.isValid(null, null)
+        //then
+        Assertions.assertTrue(valid)
+    }
+
+    @Test
+    fun shouldReturnValidIfPasswordAndPasswordConfirmIsNull() {
+        //when
+        val valid = passwordConfirmValidator.isValid(getUserDetailsDto(null, null), null)
+        //then
+        Assertions.assertTrue(valid)
+    }
+
+    @Test
+    fun shouldReturnValidIfPasswordAndPasswordConfirmMatches() {
+        //when
+        val valid = passwordConfirmValidator.isValid(getUserDetailsDto("password123", "password123"), null)
+        //then
+        Assertions.assertTrue(valid)
+    }
+
+    @Test
+    fun shouldReturnInvalidIfPasswordIsNotEmptyAndPasswordConfirmIsNull() {
+        //when
+        val valid = passwordConfirmValidator.isValid(getUserDetailsDto("password", null), null)
+        //then
+        Assertions.assertFalse(valid)
+    }
+
+    @Test
+    fun shouldReturnInvalidIfPasswordIsNullAndPasswordConfirmIsNotEmpty() {
+        //when
+        val valid = passwordConfirmValidator.isValid(getUserDetailsDto(null, "passwordConfirm"), null)
+        //then
+        Assertions.assertFalse(valid)
+    }
+
+    @Test
+    fun shouldReturnInvalidIfPasswordAndPasswordConfirmNotMatches() {
+        //when
+        val valid = passwordConfirmValidator.isValid(getUserDetailsDto("password", "passwordConfirm"), null)
+        //then
+        Assertions.assertFalse(valid)
+    }
+
+    private fun getUserDetailsDto(password: String?, passwordConfirm: String?): UserDetailsDto =
+        UserDetailsDto("name", "email", "oldPassword", password, passwordConfirm)
+
+}
