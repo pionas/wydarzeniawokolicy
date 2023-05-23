f3f21326c9a30c32da8857e9cf5323d73a0e7ba2
Adrian Pionka <adrian@pionka.com>
2023-05-23 08:12:20 +0200

    modified security config

--- src/main/kotlin/pl/wydarzeniawokolicy/infrastructure/SecurityConfig.kt
+++ src/main/kotlin/pl/wydarzeniawokolicy/infrastructure/SecurityConfig.kt
@@ -3,10 +3,8 @@ package pl.wydarzeniawokolicy.infrastructure
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.SecurityFilterChain
 
 
 @Configuration
@@ -16,16 +14,5 @@ class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
-    }
-
-    @Bean
-    fun apiFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf().disable()
-            .exceptionHandling()
-            .and()
-            .authorizeHttpRequests()
-            .anyRequest().anonymous()
-
-        return http.build()
     }
 }
